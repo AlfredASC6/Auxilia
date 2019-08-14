55f7748d8045d92c00e8e799b5258aeb968ed66f
LeomarASC6 <leomarj716@gmail.com>
2019-08-13 23:19:01 -0400

    js

--- Script.js
+++ Script.js
@@ -1,2 +1,3 @@
 let PoetryArray = ["Do not wait; the time will never be 'just right.' Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along. -George Herbert", "You are not here merely to make a living. You are here in order to enable the world to live more amply, with greater vision, with a finer spirit of hope and achievement. You are here to enrich the world, and you impoverish yourself if you forget the errand. -Woodrow Wilson", "It does not matter how slowly you go as long as you do not stop. -Confucius", "Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy. -Norman Vincent Peale"];
-Math.random();
+Math.random(4);
+Math.floor();
